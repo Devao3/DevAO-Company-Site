082e4525443d75806d9e3f915f3a5bb58cd2ebbf
AGUSTIN ORTIZ <augieortiz@me.com>
2020-01-30 09:09:03 -0500

    send mail bug fix

--- sendmail.php
+++ sendmail.php
@@ -12,7 +12,7 @@ if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject'])
   }
   
   //
-  mail( "augie@devao.me", $_POST['subject'], $_POST['name'] . $_POST['message'], "From:" . $_POST['email'] );
+  mail( "augie@devao.me", "DevAO.ME Contact", $_POST['name'] . ": " . $_POST['message'], "From:" . $_POST['email'] );
  
   //			^
   //  Replace with your email 
